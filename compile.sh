3d7ccbb718e06b34b4ef4c6f0b70410483c4fec0
Cache Makers <Image15@iMac-BB.local>
2017-02-28 17:57:54 -0700

    added hero collisions touch

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 g++ main.cpp -o temp -lsfml-system -lsfml-graphics -l sfml-window
+./temp
--- main.cpp
+++ main.cpp
@@ -10,9 +10,17 @@ int main ()
   sf::RenderWindow window(sf::VideoMode(WIDE,HIGH), "temp");
 
   sf::CircleShape circle(50);
-  circle.setFillColor(sf::Color::White );
+  circle.setFillColor(sf::Color::Red);
   circle.setPosition(100,100);
-  int circleDir = 0;
+
+  sf::RectangleShape pixie(sf::Vector2f(100,100));
+  pixie.setFillColor(sf::Color::Black);
+  pixie.setPosition(WIDE-100,HIGH-100);
+  int circleDir = 100;
+
+  int herospeed = 1;
+
+  int points = 5;
   //game loop
   while(window.isOpen() )
   {
@@ -25,6 +33,34 @@ int main ()
         window.close();
       }
     }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && pixie.getPosition().y > 0)
+        {
+          pixie.move(0,-herospeed);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && pixie.getPosition().y < HIGH-pixie.getSize().y)
+        {
+          pixie.move(0,herospeed);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)&& pixie.getPosition().x < WIDE-pixie.getSize().x)
+        {
+          pixie.move(herospeed,0);
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && pixie.getPosition().x > 0)
+        {
+          pixie.move(-herospeed,0);
+        }
+
+        if(pixie.getGlobalBounds().intersects(circle.getGlobalBounds()))
+        {
+          std::cout << "DONT TOUCH ME!!!" <<std::endl;
+        }
+
+        {
+          std::cout << "computers" << std::endl;
+        }
+
+    window.clear(sf::Color::Blue);
 
     //update
     if(circle.getPosition().x > WIDE-100)
@@ -37,16 +73,16 @@ int main ()
     }
     if(circleDir == 0)
     {
-      circle.move(.5, 0);
+      circle.move(1, 0);
     }
     else
     {
-      circle.move(-.5, 0);
+      circle.move(-1, 0);
     }
-
     //render
 
-    window.clear(sf::Color::Blue);
+
+    window.draw(pixie);
     window.draw(circle);
     window.display();
   }
